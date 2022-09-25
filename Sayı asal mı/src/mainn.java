abb7494b95c639f277ac50fc87dd3a48263335d5
Mehmet Kardaş <mehmetgskmp31@gmail.com>
2022-09-25 13:17:20 +0300

    Java Engin Demiroğ

---
+++ Sayı asal mı/src/mainn.java
@@ -0,0 +1,43 @@
+
+public class mainn {
+
+	public static void main(String[] args) {
+		
+
+		int number =23;
+		int remainder=number%2;
+		boolean isPrime =true;
+		
+		if(number==1) {
+			System.out.println("Sayı asal değildir");
+			return;
+		}
+		if(number<1) {
+			System.out.println("Geçersiz sayı");
+			return;
+		}
+		
+		for(int i=2;i<number;i++) {
+			if(number%i==0) {
+			isPrime =false;
+
+				
+				
+						}
+			
+			
+			
+		}
+		if(isPrime) {
+			
+			System.out.println("Sayı asaldır");
+		}
+		else {
+			System.out.println("Sayı asal değildir");
+		}
+		
+		
+		
+	}
+
+}
